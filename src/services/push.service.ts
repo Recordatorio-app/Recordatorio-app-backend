499fe38537672e5713ac92bd29e1d8c10b43cff2
joaoDevL <joao.software.dev@gmail.com>
2025-12-13 13:56:55 -0500

    refactor:By modifying the logic, we delete subscriptions that are no longer useful.

--- src/services/push.service.ts
+++ src/services/push.service.ts
@@ -17,8 +17,13 @@ export const sendPushToUser = async (userId: string, payloadData: any) => {
   for (const sub of subs) {
     try {
       await webpush.sendNotification(sub.subscription, payload);
-    } catch (err) {
-      console.log("❌ Error enviando push:", err);
+    } catch (err: any) {
+      console.log("❌ Error enviando push:", err.statusCode);
+
+      if (err.statusCode === 404 || err.statusCode === 410) {
+        await PushSubscription.deleteOne({ _id: sub._id });
+        console.log("🗑️ Suscripción eliminada");
+      }
     }
   }
 
