3e9415e8c323bd0df2f7fa7e17c38bae46a3b625
joaoDevL <joao.software.dev@gmail.com>
2025-12-13 10:35:55 -0500

    refactor:adding the route to retrieve the data by ID

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { register, login, updateColors, getPalette } from "../controllers/user.controller";
+import { register, login, updateColors, getPalette, getUserById } from "../controllers/user.controller";
 import { auth } from "../middleware/auth";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.get("/:id", auth, getUserById);
 // palette routes (protected - user must be owner)
 router.get("/:id/palette", auth, getPalette);
 router.put("/:id/palette", auth, updateColors);
