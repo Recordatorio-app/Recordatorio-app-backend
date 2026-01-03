99a90a80289718c8dea152e991e0825ccc91d416
joaoDevL <joao.software.dev@gmail.com>
2026-01-03 15:53:09 -0500

    feat: Adding the controller for notification

---
+++ src/controllers/notification.controller.ts
@@ -0,0 +1,39 @@
+// controllers/notification.controller.ts
+import {  Response } from "express";
+import { saveToken } from "../services/notification.service";
+import NotificationToken from "../models/NotificationToken";
+import { sendPush } from "../services/notification.service";
+import { AuthRequest } from "../middleware/auth";
+
+
+export const registerToken = async (req: AuthRequest, res: Response) => {
+  try {
+    const { token } = req.body;
+
+    // 👇 normalmente viene del middleware auth
+    const userId = req.userId;
+
+    if (!token) {
+      return res.status(400).json({ message: "Token requerido" });
+    }
+
+    await saveToken(userId, token);
+
+    res.status(201).json({ message: "Token guardado" });
+  } catch (error) {
+    res.status(500).json({ message: "Error guardando token" });
+  }
+};
+
+export const sendNotification = async (req: AuthRequest, res: Response) => {
+    if (!req.userId) {
+    return res.status(401).json({ message: "No autorizado" });
+  }
+  const { title, body } = req.body;
+  const userId = req.userId;
+  const tokens = await NotificationToken.find({ userId });
+
+  await Promise.all(tokens.map((t) => sendPush(t.token, title, body)));
+
+  res.json({ message: "Notificación enviada" });
+};
