b0a7e2ec0fbfe78dc20d6927cea73559bf84f9e6
joaoDevL <joao.software.dev@gmail.com>
2026-01-03 16:16:50 -0500

    feat: Implementing the push cron function

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,11 @@ dotenv.config();
 import connectDB from "./config/db";
 import userRoutes from "./routes/user.routes";
 import taskRoutes from "./routes/task.routes";
-import pushRoutes from "./routes/push.routes";
+import notificationRoutes from "./routes/notification.routes";
 import whatsappRoutes from "./routes/whastapp.routes";
 import {startReminders} from"./jobs/reminder.job";
+import {startPushNotifications} from "./jobs/push.job";
+
 
 const app = express();
 app.use(cors({
@@ -19,9 +21,10 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/push", pushRoutes);
 app.use("/api/whatsapp", whatsappRoutes);
+app.use("/api/notifications", notificationRoutes);
 startReminders();
+startPushNotifications();
 connectDB();
 
 export default app;
