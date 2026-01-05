86fd618703677cd29d8e2fdffcf148ed27e240a0
joaoDevL <joao.software.dev@gmail.com>
2026-01-04 23:26:48 -0500

    feat:adding the firebase library

--- src/routes/push.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { auth } from "../middleware/auth";
-import { registerSubscription } from "../controllers/push.controller";
-
-const router = Router();
-
-router.post("/subscribe", auth, registerSubscription);
-
-export default router;
