6b59b150dcbb6460983a4140dd77d915e9aac5dc
joaoDevL <joao.software.dev@gmail.com>
2026-01-06 10:22:23 -0500

    refactor: Modifying the cron job to make it work better

--- src/jobs/push.job.ts
+++ src/jobs/push.job.ts
@@ -4,50 +4,47 @@ import NotificationToken from "../models/NotificationToken";
 import { sendPush } from "../services/notification.service";
 
 export const startPushNotifications = () => {
-  cron.schedule("*/1 * * * *", async () => {
+  cron.schedule("*/10 * * * *", async () => {
+  console.log("🔔 Cron Push Notifications ejecutándose");
+  const now = new Date();
+  const tenMinutesAgo = new Date(now.getTime() - 10 * 60 * 1000);
+  const threeDaysLater = new Date(now.getTime() + 3 * 24 * 60 * 60 * 1000);
 
-    console.log("🔔 Cron push notifications ejecutándose");
-
-    const now = new Date();
-    const threeDaysLater = new Date(now.getTime() + 3 * 24 * 60 * 60 * 1000);
-
-    const tenMinutesAgo = new Date(now.getTime() - 10 * 60 * 1000);
-
-    const tasks = await Task.find({
-      status: "pendiente",
-      reminderDate: {
-        $ne: null,
-        $gte: now,
-        $lte: threeDaysLater,
-      },
-      $or: [
-        { lastReminderDate: null },
-        { lastReminderDate: { $lte: tenMinutesAgo } },
-      ],
-    });
-
-    for (const task of tasks) {
-      const tokens = await NotificationToken.find({
-        userId: task.userId,
-      });
-
-      if (!tokens.length) continue;
-
-      await Promise.all(
-        tokens.map((t) =>
-          sendPush(
-            t.token,
-            "⏰ Recordatorio de tarea",
-            `La tarea "${task.title}" vence pronto`
-          )
-        )
-      );
-
-      // actualizar control de recordatorios
-      task.lastReminderDate = now;
-      task.reminderSentDates?.push(now);
-      await task.save();
-    }
+  const tasks = await Task.find({
+    status: "pendiente",
+    reminderDate: {
+      $ne: null,
+      $gte: tenMinutesAgo,
+      $lte: threeDaysLater,
+    },
+    $or: [
+      { lastReminderDate: null },
+      { lastReminderDate: { $lte: tenMinutesAgo } },
+    ],
   });
-  console.log("✅ Push notification job activo (3 días antes, cada 10 minutos)");
+
+  console.log("📌 Tareas encontradas:", tasks.length);
+
+  for (const task of tasks) {
+    const tokens = await NotificationToken.find({ userId: task.userId });
+
+    if (!tokens.length) continue;
+
+    await Promise.all(
+      tokens.map(t =>
+        sendPush(
+          t.token,
+          "⏰ Recordatorio de tarea",
+          `La tarea "${task.title}" vence pronto`
+        )
+      )
+    );
+
+    task.lastReminderDate = now;
+    task.reminderSentDates ??= [];
+    task.reminderSentDates.push(now);
+    await task.save();
+  }
+});
+  console.log("🔔 Cron Push Notifications ejecutándose");
 };
