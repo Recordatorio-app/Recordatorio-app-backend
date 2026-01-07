02cbe6d95910aec9169842758c49b7929ba64a91
joaoDevL <joao.software.dev@gmail.com>
2026-01-06 22:53:44 -0500

    refactor:Adjusting the Firebase render function

--- src/config/fireBasead.ts
+++ src/config/fireBasead.ts
@@ -1,17 +1,12 @@
-// config/firebaseAdmin.ts
 import admin from "firebase-admin";
-
-if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT no está definida");
-}
-
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, "\n")
-);
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
